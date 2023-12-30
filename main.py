b5dc9538031edd50bce48cf7510fb596dfcffaf0
Jozef <yusufefl@gmail.com>
2023-12-30 21:10:17 +0300

    Course going on

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: int | None = None 
 
 @app.get("/")
 def root():
     return {"message":"Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"message":"Here is all posts"}
+
+@app.post("/posts")
+def create_post(new_post:Post):
+    print(new_post)
+    print(new_post.model_dump())
+    return {"new post":f"title is {new_post.title}, content is {new_post.content}"}
 
